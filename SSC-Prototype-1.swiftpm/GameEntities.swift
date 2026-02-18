6b1bba20b60fabf6fb86b82ec36510084b025e22
Azmi1180 <78455301+Azmi1180@users.noreply.github.com>
2026-02-18 11:38:02 +0700

    Add Packet Node GameEntities

--- SSC-Prototype-1.swiftpm/GameEntities.swift
+++ SSC-Prototype-1.swiftpm/GameEntities.swift
@@ -7,7 +7,6 @@
 
 import SpriteKit
 
-// 1. Define the Types
 enum PacketType {
     case video, email, malware
     
@@ -26,5 +25,33 @@ enum PacketType {
         case .email: return 150.0
         case .malware: return 200.0
         }
+    }
+}
+
+class PacketNode: SKShapeNode {
+    let type: PacketType
+    
+    init(type: PacketType) {
+        self.type = type
+        super.init()
+            
+        let size: CGFloat = 12
+        let rect = CGRect(x: -size/2, y: -size/2, width: size, height: size)
+        self.path = CGPath(rect: rect, transform: nil)
+        
+        self.fillColor = type.color
+        self.strokeColor = .white
+        self.lineWidth = 1.5
+                
+        let glow = SKShapeNode(rect: rect)
+        glow.fillColor = type.color
+        glow.strokeColor = .clear
+        glow.alpha = 0.4
+        glow.setScale(1.5)
+        addChild(glow)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 }
